23fb986cab85a2451f14b7d89c0d55b4893f654f
Maxmillion McLaughlin <github@maxmclau.com>
2016-03-04 21:58:32 -0500

    Support for remaining functions

--- gput.go
+++ gput.go
@@ -1,8 +1,9 @@
-package tput
+package gput
 
 import (
-  "github.com/codeskyblue/go-sh"
+  "log"
   "strconv"
+  "github.com/codeskyblue/go-sh"
 )
 
 func Setab(color int) () {
@@ -22,34 +23,119 @@ func Setf(color int) {
 }
 
 
-func Bold(color int) {
+
+func Bold() {
   sh.Command("tput", "bold").Run()
 }
 
-func Smul(color int) {
+func Smul() {
   sh.Command("tput", "smul").Run()
 }
 
-func Rev(color int) {
+func Rev() {
   sh.Command("tput", "rev").Run()
 }
 
-func Blink(color int) {
+func Blink() {
   sh.Command("tput", "blink").Run()
 }
 
-func Invis(color int) {
+func Invis() {
   sh.Command("tput", "invis").Run()
 }
 
-func Smso(color int) {
+func Smso() {
   sh.Command("tput", "smso").Run()
 }
 
-func Rmso(color int) {
+func Rmso() {
   sh.Command("tput", "rmso").Run()
 }
 
-func Sgr0(color int) {
+func Sgr0() {
   sh.Command("tput", "sgr0").Run()
+}
+
+
+
+func Cup(x int, y int) {
+  sh.Command("tput", "cup", strconv.Itoa(y), strconv.Itoa(x)).Run()
+}
+
+func Sc() {
+  sh.Command("tput", "sc").Run()
+}
+
+func Rc() {
+  sh.Command("tput", "rc").Run()
+}
+
+func Lines()(string) {
+  lines, err := sh.Command("tput", "lines").Output()
+  if err != nil {
+    log.Fatal(err)
+  }
+  return string(lines)
+}
+
+func Cols()(string) {
+  cols, err := sh.Command("tput", "cols").Output()
+  if err != nil {
+    log.Fatal(err)
+  }
+  return string(cols)
+}
+
+func Cub(dist int) {
+  sh.Command("tput", "cub", strconv.Itoa(dist)).Run()
+}
+
+func Cuf(dist int) {
+  sh.Command("tput", "cuf", strconv.Itoa(dist)).Run()
+}
+
+func Cub1() {
+  sh.Command("tput", "cub1").Run()
+}
+
+func Cuf1() {
+  sh.Command("tput", "cuf1").Run()
+}
+
+func Ll() {
+  sh.Command("tput", "ll").Run()
+}
+
+func Cuu1() {
+  sh.Command("tput", "cuu1").Run()
+}
+
+
+
+func Ech(chars int) {
+  sh.Command("tput", "ech", strconv.Itoa(chars)).Run()
+}
+
+func Clear() {
+  sh.Command("tput", "clear").Run()
+}
+
+func El1() {
+  sh.Command("tput", "el1").Run()
+}
+
+func El() {
+  sh.Command("tput", "el").Run()
+}
+
+func Ed() {
+  sh.Command("tput", "ed").Run()
+}
+
+func Ich(chars int) {
+  sh.Command("tput", "ich", strconv.Itoa(chars)).Run()
+}
+
+func Il(lines int) {
+  sh.Command("tput", "il", strconv.Itoa(lines)).Run()
 }
